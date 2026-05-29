ef5ec35e87a44de233c24c69e167b6d53565e549
Paulo Henrique <paulohenriquedev2026@gmail.com>
2026-05-29 15:16:38 -0500

    Mudando umas coias
    parece que o programa salvou  errado, agora deu certo

--- Mundo03/extra1.py
+++ Mundo03/extra1.py
@@ -4,7 +4,7 @@
 cidade_brasileiras = ('Acre', 'Bahia', 'Rio de janeiro', 'São paulo', 'Salvador')
 
 nome_de_uma_cidade = input('Informe o nome de uma cidade brasileira: ').strip().capitalize()
-if nome_de_uma_cidade in cidade_brasileiras:
+if nome_de_uma_cidade in cidade_brasileiras: # verifica se o nome escolhido está dentro da tupla
     print(f'A cidade {nome_de_uma_cidade} está no indice {cidade_brasileiras.index(nome_de_uma_cidade)}')
 else:
     print('Cidade não foi encontrada')
--- Mundo03/extra2.py
+++ Mundo03/extra2.py
@@ -1,10 +1,17 @@
+# Cria um tupla com dez números inteiros.
+# mostra o maior, menor e a soma entre eles
+# verifica se um número digitado está dentro da tupla e mostra o índice
+
 from random import randint
  
 num =  tuple(randint(0,10) for c in range(10)) 
-numero_digitado = int(input('Informe um número: '))
+numero_digitado = int(input('Informe um número (entre 0 e 10): '))
 
 if numero_digitado in num:
     print(f'Número digitado {numero_digitado} está na posicão {num.index(numero_digitado)}')
+else:
+    print('O número não esta na tupla')
+
 print(f'\nMaior número foi {max(num)}')
 print(f'Menor número foi {min(num)}')
 print(f'Soma de todos eles foram {sum(num)}')
